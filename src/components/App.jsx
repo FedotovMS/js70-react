892b197c04bd56be22627774034ef003bc756299
Mykola Fedotov <fedotovns@gmail.com>
2023-08-07 23:36:31 +0200

    user profile created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
+import user from './data/user.json';
+import Profile from './Profile/Profile';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    // user profile
+    <>
+      <>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </>
+    </>
   );
 };
+
+export default App;
